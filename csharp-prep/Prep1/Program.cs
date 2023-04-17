79236c779c074090ade3b1d38180cffe5dd18c91
kellyo-smith <smi22002@byui.edu>
2023-04-17 12:08:11 -0500

    grade Percentage

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -11,5 +11,58 @@ class Program
         string lname = Console.ReadLine();
 
         Console.WriteLine($"Your name is {lname}, {fname} {lname}");
+
+
+        //A >= 90
+        //B >= 80
+        //C >= 70
+        //D >= 60
+        //F < 60
+
+        Console.Write("What is your grade percentage?  ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+            //Console.WriteLine("Your grade is an A");
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+            //Console.WriteLine("Your grade is a B");
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+            //Console.WriteLine("Your grade is a C");
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+            //Console.WriteLine("Your grade is a D");
+        }
+        else
+        {
+            letter = "F";
+            //Console.WriteLine("Your grade is an F");
+
+        }
+
+
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed! :)");
+        }
+        else
+        {
+            Console.WriteLine("You failed :(  Better luck next time!");
+        }
     }
 }
